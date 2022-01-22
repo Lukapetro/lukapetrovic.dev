fb90c192a6a5f479ddcaead9e91effaa770812ae
Petrovic Luka (Consulente) <luka.petrovic@consulenti.sella.it>
2022-01-22 19:47:03 +0100

    projects

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -11,20 +11,22 @@ export default function ProjectCard({
   return (
     <Link href={`/projects/${slug}`}>
       <a
-        className="border-4 border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-white dark:bg-gray-900"
+        className="flex border-4 border-grey-200 dark:border-gray-800 rounded p-4 w-full bg-white dark:bg-gray-900"
         {...rest}
       >
         <Image
           alt={title}
-          height={32}
-          width={32}
+          height={64}
+          width={64}
           src={`/logos/${logo}`}
           className="rounded-full"
         />
-        <h3 className="text-lg font-bold text-left mt-2 text-gray-900 dark:text-gray-100">
-          {title}
-        </h3>
-        <p className="mt-1 text-gray-700 dark:text-gray-400">{description}</p>
+        <div className="flex flex-col ml-8 ">
+          <h3 className="text-lg font-bold text-left mt-2 text-gray-900 dark:text-gray-100">
+            {title}
+          </h3>
+          <p className="mt-1 text-gray-700 dark:text-gray-400">{description}</p>
+        </div>
       </a>
     </Link>
   );
