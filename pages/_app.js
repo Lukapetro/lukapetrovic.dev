7ea2e7d043df2f4d950bbfff8997703f4fac513f
Petrovic Luka (Consulente) <luka.petrovic@consulenti.sella.it>
2022-01-23 10:27:04 +0100

    maintenance

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import 'styles/globals.css'
 
+import Maintenance from './maintenance';
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
+  if (process.env.NEXT_PUBLIC_MAINTENANCE === 'true') {
+    return <Maintenance />;
+  }
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
---
+++ pages/maintenance.jsx
@@ -0,0 +1,9 @@
+export default function Maintenance() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-2xl	font-bold">
+        lukapetrovic.dev is under maintenance.
+      </h1>
+    </div>
+  );
+}
