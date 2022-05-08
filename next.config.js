2a031d4464e1c27a6787b842e1aea6e9791a0ff6
Luka Petrovic <Lukapetro@yahoo.it>
2022-05-08 09:19:10 +0200

    feat: package upgrade

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const { withContentlayer } = require('next-contentlayer');
 const { version } = require('./package.json');
 
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = withContentlayer()({
+module.exports = withContentlayer({
   swcMinify: true,
   reactStrictMode: true,
   publicRuntimeConfig: {
