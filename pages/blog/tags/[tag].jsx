f452a7121f18c70592159389e05583b49b68161e
lukapetro <lucapetro93@gmail.com>
2022-06-29 10:55:03 +0200

    :sparkles: feat(ui): :sparkles: feat(ui): tags back

--- pages/blog/tags/[tag].jsx
+++ pages/blog/tags/[tag].jsx
@@ -1,22 +1,23 @@
 import BlogPost from 'components/BlogPost';
 import Container from 'components/Container';
 import { allBlogs } from '.contentlayer/generated';
-import Link from 'next/link';
 import BackButton from 'components/BackButton';
 
 export default function BlogTag({ posts, tag }) {
+  if (!posts)
+    return (
+      <p className="mb-4 text-gray-600 dark:text-gray-400">
+        There are no posts.
+      </p>
+    );
+
   return (
     <Container pageTitle={`#${tag}`}>
       <div className="flex gap-8">
-        <BackButton href={'/blog'} label="Back to all posts" />
-        <BackButton href={'/blog/tags'} label="Back to all #Tags" />
+        <BackButton href={'/blog'} label="Posts" />
+        <BackButton href={'/blog/tags'} label="#Tags" />
       </div>
 
-      {!posts.length && (
-        <p className="mb-4 text-gray-600 dark:text-gray-400">
-          There are no posts.
-        </p>
-      )}
       <div className="mt-4 w-full">
         {posts.map((post) => (
           <BlogPost key={post.title} {...post} />
--- pages/blog/tags/index.jsx
+++ pages/blog/tags/index.jsx
@@ -2,32 +2,15 @@ import Container from 'components/Container';
 import ALL_TAGS from 'utils/Tags.json';
 import TagsCard from 'components/tags/TagsCard';
 import { allBlogs } from '.contentlayer/generated';
-import Link from 'next/link';
+import BackButton from 'components/BackButton';
 
 export default function Blog({ count }) {
   const { tags } = ALL_TAGS;
 
   return (
     <Container pageTitle="Tags">
-      <Link href={'/blog'}>
-        <a className="flex text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6 justify-center items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4 mr-2"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10 19l-7-7m0 0l7-7m-7 7h18"
-            />
-          </svg>
-          Back to all posts
-        </a>
-      </Link>
+      <BackButton href={'/blog'} label="Posts" />
+
       <div className="mt-4 w-full grid grid-cols-1 md:grid-cols-2 gap-4">
         {tags.map((tag) => (
           <TagsCard key={tag.title} tag={tag} count={count[tag.title]} />
