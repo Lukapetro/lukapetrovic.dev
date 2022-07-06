a8b22db18818021d98e311db4bfdf8f833d129e3
lukapetro <lucapetro93@gmail.com>
2022-07-06 18:25:46 +0200

    fix: overflow auto aside

--- components/mdx/Aside.jsx
+++ components/mdx/Aside.jsx
@@ -1,6 +1,6 @@
 export default function Aside({ emoji, children }) {
   return (
-    <div className="bg-gray-600 rounded-lg">
+    <div className="bg-gray-600 rounded-lg overflow-auto">
       <div className="max-w-7xl py-3 px-3 sm:px-6 ">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
